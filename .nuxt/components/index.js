aba3388c8f853f6d4eddd412bbf560522d040115
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2020-10-09 10:57:50 +0700

    delete .nuxt dir

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from '../../components/Logo.vue'
-export { default as VirusImage } from '../../components/covid/virus-image.vue'
-
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
-export const LazyVirusImage = import('../../components/covid/virus-image.vue' /* webpackChunkName: "components/covid/virus-image" */).then(c => c.default || c)
