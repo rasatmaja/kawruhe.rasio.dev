040939f11b52e5e3d41feb66567368542b866e41
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2020-10-18 12:46:39 +0700

    change color on loading animation

--- components/covid/CovidLoading.vue
+++ components/covid/CovidLoading.vue
@@ -21,26 +21,23 @@
   height: 13px;
   border-radius: 50%;
   animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  @apply bg-covid-gray-primary;
 }
 .lds-ellipsis div:nth-child(1) {
   left: 8px;
   animation: lds-ellipsis1 0.6s infinite;
-  @apply bg-covid-blue-primary;
 }
 .lds-ellipsis div:nth-child(2) {
   left: 8px;
   animation: lds-ellipsis2 0.6s infinite;
-  @apply bg-covid-green-primary;
 }
 .lds-ellipsis div:nth-child(3) {
   left: 32px;
   animation: lds-ellipsis2 0.6s infinite;
-  @apply bg-covid-red-primary;
 }
 .lds-ellipsis div:nth-child(4) {
   left: 56px;
   animation: lds-ellipsis3 0.6s infinite;
-  @apply bg-covid-gray-primary;
 }
 @keyframes lds-ellipsis1 {
   0% {
