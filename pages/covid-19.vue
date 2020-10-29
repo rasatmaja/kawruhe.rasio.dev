1e2a2fc5df7616db895aed25a5c0322fa78ff43d
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2020-10-29 12:06:01 +0700

    remove noscript

--- pages/covid-19.vue
+++ pages/covid-19.vue
@@ -519,11 +519,6 @@ export default {
           onload: 'this.media=\'all\''
         }
       ],
-      noscript: [
-        {
-          innerHTML: '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@700&display=swap">'
-        }
-      ],
       meta: [
         {
           hid: 'description',
